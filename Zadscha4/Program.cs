65171292132a52b079e80fcd5af56347de799ae8
Margarita <diasimba2014@gmail.com>
2023-04-13 00:25:39 +0300

    Zadacha4

---
+++ Zadscha4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введитe третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c)
+{
+    Console.WriteLine (a);
+}
+if (b > a && b > c)
+{
+    Console.WriteLine (b);
+}
+if (c > a && c > b)
+{
+    Console.WriteLine (c);  
+}
