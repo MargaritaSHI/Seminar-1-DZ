6abf03b4118865866ea1af47b95348e6d1339e03
Margarita <diasimba2014@gmail.com>
2023-04-13 21:14:21 +0300

    Zadacha8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int number = 2;
+
+if (N > 1);
+{
+    while(number <= N)
+    
+    {
+        Console.WriteLine(number + "");
+        number = number + 2;
+    }
+}
